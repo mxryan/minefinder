bf943153ac712d96cfcf11d2d0a6b65d0699f16a
M Ryan <mxryan@live.com>
2018-11-14 15:53:30 -0500

    initial react files

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+        <p>WAZZZZZZZAHHHHHHHH</p>
+      </div>
+    );
+  }
+}
+
+export default App;
