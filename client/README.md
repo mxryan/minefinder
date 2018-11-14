0b9576132e9c2570b4c2817974acc02ceac1503c
M Ryan <mxryan@live.com>
2018-11-14 15:41:59 -0500

    wiped out client director

--- client/README.md
+++
@@ -1,2 +0,0 @@
-# reactminefinder
-minesweeper clone made with react. This will hopefully later be integrated with an express backend and hosted on heroku.
---
+++ client/build/precache-manifest.0765c5fae7ab0168be77a74d969aa06f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad96c8143a41a27cdfc5",
+    "url": "/reactminefinder/static/js/runtime~main.ad96c814.js"
+  },
+  {
+    "revision": "7bb00f8e18791b16abaa",
+    "url": "/reactminefinder/static/js/main.7bb00f8e.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/reactminefinder/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "59a05ae768bed4be28e9b60a2d8cc14d",
+    "url": "/reactminefinder/index.html"
+  }
+];
