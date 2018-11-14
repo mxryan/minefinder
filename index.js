4720f53c347775eadb8a73ca72a8078d78df580a
M Ryan <mxryan@live.com>
2018-11-14 15:09:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("/api/example", (req, res)=>{
+  res.json({greeting: "hello world"});
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>console.log("listening on port 5000"));
