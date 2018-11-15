da3c470712d25a0558a8f3c0053e3b038054319f
M Ryan <mxryan@live.com>
2018-11-15 18:15:26 -0500

    signup page works

--- client/src/App.js
+++ client/src/App.js
@@ -5,34 +5,56 @@ import Nav from "./components/Nav";
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       loggedIn: false,
-      page: "welcome"
+      page: "welcome",
+      signupUsername: "",
+      signupPassword: ""
     }
   }
-  changePage = (e)=>{
+  changePage = (e) => {
     let name = e.target.getAttribute("name");
     this.setState({
       page: name
     });
   }
+
+  handleFormChange = (e) => {
+    let name = e.target.id;
+    this.setState({ [name]: e.target.value });
+    console.log(this.state);
+  }
+
+  submitSignup = ()=>{
+    let outData = {
+      username: this.state.signupUsername,
+      password: this.state.signupPassword
+    }
+    fetch("/api/signup", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(outData)
+  }).then(r => r.json()).then(d=>console.log(d)).catch(e=>console.log(e));
+  }
   render() {
     let page;
-    switch(this.state.page) {
+    switch (this.state.page) {
       case "welcome":
-      page = (<div><h1>Welcome!</h1></div>);
-      break;
+        page = (<div><h1>Welcome!</h1></div>);
+        break;
       case "signup":
-      page = (<Signup />)
-      break;
+        page = (<Signup handleChange={this.handleFormChange} submitSignup={this.submitSignup} />)
+        break;
       default:
-      page = (<Game />);
+        page = (<Game />);
     }
     return (
       <div>
-        <Nav changePage={this.changePage}/>
+        <Nav changePage={this.changePage} />
         {page}
       </div>
     );
--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -5,12 +5,12 @@ const Signup = (props) => {
     <div>
       <h1>This is the signup page</h1>
       <p>Sign up here!!!!!!</p>
-      <label htmlFor="email">Email</label>
-      <input type="text" onChange={props.handleChange} id="email"></input>
-      <label htmlFor="username">Username</label>
-      <input type="text" onChange={props.handleChange} id="username"></input>
-      <label htmlFor="password">Password</label>
-      <input type="password" onChange={props.handleChange} id="password"></input>
+
+      <label htmlFor="signupUsername">Username</label>
+      <input type="text" onChange={props.handleChange} id="signupUsername"></input>
+      <label htmlFor="signupPassword">Password</label>
+      <input type="password" onChange={props.handleChange} id="signupPassword"></input>
+      <button onClick={props.submitSignup}></button>
     </div>
   )
 }
