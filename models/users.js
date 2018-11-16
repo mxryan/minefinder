8cdf2988d9f3dbd21cd1be5f9518b398697a3c0c
M Ryan <mxryan@live.com>
2018-11-16 12:08:39 -0500

    added hashing hook and password comparision to user model

--- models/users.js
+++ models/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt-nodejs")
 module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define("Users", {
     id: {
@@ -34,6 +35,15 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: true
     }
+  });
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  Users.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  Users.hook("beforeCreate", function (user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
   return Users;
 }
