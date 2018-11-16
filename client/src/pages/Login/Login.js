64f85fd3fb93900537dcac846cbdb6d96e1965dd
M Ryan <mxryan@live.com>
2018-11-16 13:59:51 -0500

    added login page

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Login = (props) => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Hey man this is the login page, which you will visit to login after you sign up. The should also be something on the nav bar that lets you know if you are signed in. I'm also gonna put a button that sends a json response based on whether or not you are logged in.</p>
+    </div>
+  )
+}
