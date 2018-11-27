0847ff1af40f7cdcf4a4c1ec4d5870b76ccad9a9
M Ryan <mxryan@live.com>
2018-11-27 18:55:23 -0500

    changed small board bug

--- client/src/components/ControlPanel/ControlPanel.js
+++ client/src/components/ControlPanel/ControlPanel.js
@@ -2,9 +2,9 @@ import React from "react";
 const ControlPanel = (props) => {
   return (
     <div>
-      Board size: &nbsp;
+      Size: &nbsp;
       <button name="small" onClick={props.changeBoardSize}>Small</button>
-      <button name="medium" onClick={props.changeBoardSize}>Medium</button>
+      <button name="medium" onClick={props.changeBoardSize}>Med</button>
       <button name="large" onClick={props.changeBoardSize}>Large</button>
       &nbsp; &nbsp;
       {props.intervalId
